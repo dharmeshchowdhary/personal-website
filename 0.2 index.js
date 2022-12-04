a7d997f77dbae2baeeed5fba9901459ffba2e05c
dharmesh_chowdhary <dharmeshchowdhary2000@gmail.com>
2022-12-04 16:45:51 +0530

    First commit

---
+++ 0.2 index.js
@@ -0,0 +1,17 @@
+myheader.innerHTML = `
+    <div class="navbar">
+        <span class="logo">
+            <a href="0.0 index.html" class="logo"><img src="#" alt="logo" class="logo">DC</a>
+        </span>
+        <span class="links">
+            <ul>
+                <li><a href="1.0 Portfolio.html">Portfolio</a></li>
+                <li><a href="2.0 Community.html">Community</a></li>
+                <li><a href="3.0 Connect.html">Connect</a></li>
+            </ul>
+        </span>
+        <span class="dark-mode">
+            <img src="#" alt="dark-mode" class="dark-mode">Dark
+        </span>
+    </div>
+`
