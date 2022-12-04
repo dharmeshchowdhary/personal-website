2dde66cdece1c5f2acc91be31650b676074c1e48
dharmesh_chowdhary <dharmeshchowdhary2000@gmail.com>
2022-12-04 17:53:19 +0530

    Renamed

---
+++ Webpages/0.2 index.js
@@ -0,0 +1,17 @@
+myheader.innerHTML = `
+    <div class="navbar">
+        <span class="logo">
+            <img src="#" alt="logo" class="logo"><a href="/index.html" class="logo">DC</a>
+        </span>
+        <span class="links">
+            <ul>
+                <li><a href="/Webpages/1.0 Portfolio.html">Portfolio</a></li>
+                <li><a href="/Webpages/2.0 Community.html">Community</a></li>
+                <li><a href="/Webpages/3.0 Connect.html">Connect</a></li>
+            </ul>
+        </span>
+        <span class="dark-mode">
+            <img src="#" alt="dark-mode" class="dark-mode">Dark
+        </span>
+    </div>
+`
