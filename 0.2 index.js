2bf4e2040c80f8ab6ca7635910fb584f46ff547e
dharmesh_chowdhary <dharmeshchowdhary2000@gmail.com>
2022-12-13 12:17:29 +0530

    Ignore

---
+++ 0.2 index.js
@@ -0,0 +1,65 @@
+document.onkeydown = function (cc) {
+    if(cc.which == 85){
+        return false;
+    }
+}
+
+document.addEventListener("contextmenu", (e) => {
+  e.preventDefault();
+}, false);
+
+document.addEventListener("keydown", (e) => {
+  // USE THIS TO DISABLE CONTROL AND ALL FUNCTION KEYS
+  // if (e.ctrlKey || (e.keyCode>=112 && e.keyCode<=123)) {
+  // THIS WILL ONLY DISABLE CONTROL AND F12
+  if (e.ctrlKey || e.keyCode==123) {
+    e.stopPropagation();
+    e.preventDefault();
+  }
+});
+
+a = myheader.innerHTML = `
+    <div class="navbar">
+        <span class="logo">
+            <img src="#" alt="logo" class="logo-image">
+            <a href="index.html" class="logo-name"><text class="dharmesh">Dharmesh</text>Chowdhary</a>
+        </span>
+        <span class="links">
+            <ul>
+                <li><a href="1.0 Portfolio.html">Portfolio</a></li>
+                <li><a href="2.0 Community.html">Community</a></li>
+                <li><a href="3.0 Connect.html">Connect</a></li>
+            </ul>
+        </span>
+        <span class="dark-mode">
+            <img src="#" alt="dark-mode" class="dark-mode">Dark
+        </span>
+    </div>
+`;
+
+b = myfooter.innerHTML = `
+    <hr>
+    <div class="footer">
+        <div class="social-links footer-innerdivs">
+            <h1>Social</h1>
+            <ul>
+        <!--    <li><a href="#" target="blank">Instagram</a></li>
+                <li><a href="#" target="blank">Twitter</a></li>    -->
+                <li><a href="https://www.linkedin.com/in/dharmesh-chowdhary/" target="blank">LinkedIn</a></li>
+        <!--    <li><a href="#" target="blank">WhatsApp</a></li>
+                <li><a href="#" target="blank">e-mail</a></li>    -->
+            </ul>
+        </div>
+        <div class="other-links footer-innerdivs">
+            <h1>Other links</h1>
+            <ul>
+                <li><a href="#" target="blank">Start-up</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="info">
+        <h2>Made with ❤ by Dharmesh Chowdhary</h2>
+        <text>copyright stuff</text>
+    </div>
+    <hr>
+`;
