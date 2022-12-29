d8965270122271d651edd9d66b97e406c20f8727
dharmesh_chowdhary <dharmeshchowdhary2000@gmail.com>
2022-12-29 10:58:53 +0530

    Update

--- 0.2 index.js
+++ 0.2 index.js
@@ -21,7 +21,7 @@ document.addEventListener("keydown", (e) => {
 a = myheader.innerHTML = `
     <div class="navbar">
         <span class="logo">
-            <img src="#" alt="logo" class="logo-image">
+            <img src="Extras/Logo/DC.png" width="100rem" height="100rem" alt="logo" class="logo-image">
             <a href="index.html" class="logo-name"><text class="dharmesh">Dharmesh</text>Chowdhary</a>
         </span>
         <span class="links">
@@ -46,6 +46,7 @@ b = myfooter.innerHTML = `
         <!--    <li><a href="#" target="blank">Instagram</a></li>
                 <li><a href="#" target="blank">Twitter</a></li>    -->
                 <li><a href="https://www.linkedin.com/in/dharmesh-chowdhary/" target="blank">LinkedIn</a></li>
+                <li><a href="https://github.com/dharmeshchowdhary" target="blank">GitHub</a></li>
         <!--    <li><a href="#" target="blank">WhatsApp</a></li>
                 <li><a href="#" target="blank">e-mail</a></li>    -->
             </ul>
@@ -53,13 +54,15 @@ b = myfooter.innerHTML = `
         <div class="other-links footer-innerdivs">
             <h1>Other links</h1>
             <ul>
-                <li><a href="#" target="blank">Start-up</a></li>
+                <!-- <li><iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeB57Kt8XaDfTEoF6ZoI66-1HNgFRNLpHaxMyNqxE58wlEMjg/viewform?embedded=true" width="640" height="1625" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe></li> -->
+                <li><a href="https://forms.gle/2A3Vsq1YBTF1uimH7" target="blank">Start-up</a></li>
             </ul>
         </div>
     </div>
     <div class="info">
         <h2>Made with ❤ by Dharmesh Chowdhary</h2>
-        <text>copyright stuff</text>
+        <text>Using HTML, CSS, JavaScript</text>
+        <text>Owner of the project and the code</text>
     </div>
     <hr>
 `;
